ce9ab00655c4912806c962afa1dbdc60265b1c22
joeyGumer <pep3553@gmail.com>
2016-02-28 13:00:45 +0100

    Animation and Collision added

--- Motor2D/j1Scene.cpp
+++ Motor2D/j1Scene.cpp
@@ -15,12 +15,8 @@ j1Scene::j1Scene() : j1Module()
 {
 	name.create("scene");
 
-	window =  NULL;
-	slider = NULL;
-	value_label = NULL;
-		/*
-	button = NULL;
-	button_title = window_title = NULL;
+	/*
+	Nullifie pointer here o at the class
 	*/
 }
 
@@ -40,6 +36,10 @@ bool j1Scene::Awake()
 // Called before the first frame
 bool j1Scene::Start()
 {
+	/*
+	Select map here (this will be transfered to an xml
+	*/
+
 	if(App->map->Load("iso_walk.tmx") == true)
 	{
 		int w, h;
@@ -52,49 +52,13 @@ bool j1Scene::Start()
 
 	debug_tex = App->tex->Load("maps/path2.png");
 	
+	//---------
 	
-	//Gui Element
-	window = App->gui->AddGuiImage({ 350, 60 }, { 17, 514, 448, 497 }, NULL, this);
-	window->interactable = true;
-	window->draggable = true;
-	window->mask = true;
-
 	/*
-	button = App->gui->AddGuiImage({ 110 , 300 }, { 642, 169, 229, 69 }, window, this);
-	button->interactable = true;
-	button->focusable = true;
-	button->draggable = true;
-	button->Center(true, false);
-
-	input = App->gui->AddGuiInputBox("Your name", NULL, { 50, 100 }, 315, { 488, 569, 344, 61 }, { -13, -14 }, window, this);
-	input->interactable = true;
-	input->focusable = true;
-	input->draggable = true;
-	input->Center(true, false);
-
-	input2 = App->gui->AddGuiInputBox("Your name", NULL, { 50, 100 }, 315, { 488, 569, 344, 61 }, { -13, -14 }, window, this);
-	input2->interactable = true;
-	input2->focusable = true;
-	input2->draggable = true;
-	input2->password = true;
-	input2->Center(true, true);
-
-	window_title = App->gui->AddGuiLabel("Window", NULL, { 200, 50 }, window, this);
-
-	button_title = App->gui->AddGuiLabel("Button", NULL, { 95, 20 }, button, this);
+	Gui elements created here
 	*/
 
-	//EXERCISE 1
-	slider = App->gui->AddGuiSlider({ 0, 0 }, { 0, 11, 307, 11 }, { 805, 318, 26, 15 }, 297, -2, { -5, 0 }, 1.0f, window, this);
-	slider->Center(true, true);
-	slider->interactable = true;
-	slider->focusable = true;
 	//---------
-
-	//EXERCISE 5
-	p2SString value_string("Value : %.3f", slider->GetValue());
-	value_label = App->gui->AddGuiLabel(value_string.GetString(), NULL, { 0, 50 }, window, this);
-	value_label->Center(true, false);
 
 	return true;
 }
@@ -102,7 +66,9 @@ bool j1Scene::Start()
 // Called each loop iteration
 bool j1Scene::PreUpdate()
 {
-
+	/*
+	This will go out
+	*/
 	// debug pathfing ------------------
 	static iPoint origin;
 	static bool origin_selected = false;
@@ -126,7 +92,6 @@ bool j1Scene::PreUpdate()
 		}
 	}
 
-
 	return true;
 }
 
@@ -136,6 +101,10 @@ bool j1Scene::Update(float dt)
 	//Gui
 	
 	// -------
+
+	/*
+	This inputs will be totally changed
+	*/
 	if(App->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN)
 		App->LoadGame("save_game.xml");
 
@@ -153,9 +122,13 @@ bool j1Scene::Update(float dt)
 
 	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
 		App->render->camera.x -= floor(200.0f * dt);
+	//
 
 	App->map->Draw();
 
+	/*
+	This will be for debuguing too
+	*/
 	int x, y;
 	App->input->GetMousePosition(x, y);
 	iPoint map_coordinates = App->map->WorldToMap(x - App->render->camera.x, y - App->render->camera.y);
@@ -165,8 +138,9 @@ bool j1Scene::Update(float dt)
 					App->map->data.tilesets.size(),
 					map_coordinates.x, map_coordinates.y);
 
-	//App->win->SetTitle(title.GetString());
-
+	/*
+	TO CHANGE
+	*/
 	// Debug pathfinding ------------------------------
 	//int x, y;
 	App->input->GetMousePosition(x, y);
@@ -183,9 +157,9 @@ bool j1Scene::Update(float dt)
 		iPoint pos = App->map->MapToWorld(path->At(i)->x, path->At(i)->y);
 		App->render->Blit(debug_tex, pos.x, pos.y);
 	}
+	//---------------
 
-
-	// Gui ---
+	
 	return true;
 }
 
@@ -210,49 +184,8 @@ bool j1Scene::CleanUp()
 
 void j1Scene::OnEvent(GuiElement* element, GUI_Event even)
 {	
-	/*if (button == element)
-	{
-		switch (even)
-		{
-		case EVENT_MOUSE_LEFTCLICK_DOWN:
-			element->SetTextureRect({ 411, 169, 229, 69 });
-			break;
-		case EVENT_MOUSE_LEFTCLICK_UP:
-			element->SetTextureRect({ 0, 113, 229, 69 });
-			break;
-		case EVENT_MOUSE_RIGHTCLICK_DOWN:
-			break;
-		case EVENT_MOUSE_RIGHTCLICK_UP:
-			break;
-		case EVENT_MOUSE_ENTER:
-		case EVENT_FOCUS_DOWN:
-			element->SetTextureRect({ 0, 113, 229, 69 });
-			break;
-		case EVENT_MOUSE_EXIT:
-		case EVENT_FOCUS_UP:
-			element->SetTextureRect({ 642, 169, 229, 69 });
-			break;
-		}
-	}*/
-	//EXERCISE 4
-	if (slider == element)
-		switch (even)
-	{
-		case EVENT_MOUSE_LEFTCLICK_DOWN:
-			slider->clicking = true;
-			break;
-		case EVENT_MOUSE_LEFTCLICK_UP:
-			slider->clicking = false;
-			break;
-		//EXERCISE 5
-		case EVENT_SLIDER_CHANGE:
-		{
-			p2SString value_string("Value : %0.2f", slider->GetValue());
-			value_label->SetText(value_string);
-		}
-		break;
-	}
-
-	
+	/*
+	Put UI element relevant to scene here
+	*/
 	//----
 }
